4311fa9a6ed51b3a49b0bea1546981606efcf77f
YangJiao1996 <kjcjwj007@gmail.com>
2019-05-11 23:43:19 -0400

    Script for HED test

--- test/edge_nms.m
+++ test/edge_nms.m
@@ -1,9 +1,12 @@
 clear; clc;
 
+
+path_to_toolbox = '/users/guest418/scratch/toolbox.badacost.public';
 path_to_pdollar = '/users/guest418/scratch/edges';
-path_to_input = '/users/guest418/scratch/HED-pytorch/pytorch-hed/test';
+path_to_input = '/users/guest418/scratch/HED-pytorch/pytorch-hed/BSR/BSDS500/data/images/out';
 path_to_output = '/users/guest418/scratch/BSR/bench/data/test-hed';
 
+addpath(genpath(path_to_toolbox));
 addpath(genpath(path_to_pdollar));
 if ~exist(path_to_output, 'dir')
     mkdir(path_to_output);
@@ -12,7 +15,7 @@ end
 iids = dir(fullfile(path_to_input, '*.jpg'));
 for i = 1:length(iids)
     edge = imread(fullfile(path_to_input, iids(i).name));
-    edge = 1-single(edge)/255;
+    edge = single(edge)/255;
 
     [Ox, Oy] = gradient2(convTri(edge, 4));
     [Oxx, ~] = gradient2(Ox);
@@ -21,6 +24,6 @@ for i = 1:length(iids)
     % 2 for BSDS500 and Multi-cue datasets, 4 for NYUD dataset
     edge = edgesNms(edge, O, 2, 5, 1.01, 8);
     
-    imwrite(edge, fullfile(path_to_output, [iids(i).name(1:end-4) '.png']));
+    imwrite(edge, fullfile(path_to_output, [iids(i).name(5:end-4) '.png']));
     
 end
